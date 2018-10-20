545b732030a79e8c40585844c5b1b6f2c336a500
Kavita Ganesan <kavgan@github.com>
2018-10-20 16:00:51 -0600

    changes
    changes

--- word_cloud.py
+++ word_cloud.py
@@ -1,84 +1,149 @@
 import pandas as pd
 import math
 import random
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 
-html=[]
-words_colors=[]
 
-color_choices=['#8000FF',
-               '#3498DB',
-               '#FF5733',
-               '#223AE6',
-               '#2ECC71',
-               '#5F6A6A',
-               '#6C22E6',
-               '#CE22E6',
-               '#ACB02E',
-               '#B18904',
-               '#848484',
-               '#04B404',
-               '#5882FA',
-               '#FF0080',
-               '#0489B1',
-               '#FA5858',
-               '#DBA901',
-               '#00b4ff',
-               '#008080',
-               '#003366',
-               '#725394'
-               ]
+class WordCloud:
 
-#randomize color choices
-#random.shuffle(color_choices)
-    
+    def __init__(self):
+        self.data = []
+        self.color_choices = ['#b82c2c',
+                              '#a55571',
+                              '#bc72d0',
+                              '#8000FF',
+                              '#3498DB',
+                              '#FF5733',
+                              '#223AE6',
+                              '#2ECC71',
+                              '#5F6A6A',
+                              '#6C22E6',
+                              '#CE22E6',
+                              '#ACB02E',
+                              '#B18904',
+                              '#848484',
+                              '#04B404',
+                              '#5882FA',
+                              '#FF0080',
+                              '#0489B1',
+                              '#FA5858',
+                              '#DBA901',
+                              '#00b4ff',
+                              '#008080',
+                              '#003366',
+                              '#725394'
+                              ]
 
-def get_color_code(score):   
-    
-    step=0.05
-    start=0
-    idx=0
-    
-    while start<1:
-        if score<=start:
-            return color_choices[idx]
-        idx+=1
-        start=start+step
-        
-    return color_choices[0]
-    
-    
-    
-def get_font_size(score):
-    # font size start and increment
-    scale=0.6
-    scale_step=0.3
-    
-    #score increment
-    score_step=0.05
-    start=0
-    idx=0
-    
-    
-    while start<1:
-        if score<=start:
-            return scale
-        start=start+score_step
-        scale+=scale_step
+    def get_color_code(self, score):
+        step = 0.05
+        current_incremented_score = 0
+        idx = 0
 
-def get_embed_code(df:pd.DataFrame):
-    random.shuffle(color_choices)
-    html=[]
-    words_colors=[]
+        while current_incremented_score < 1:
+            if score <= current_incremented_score:
+                return self.color_choices[idx]
+            idx += 1
+            current_incremented_score = current_incremented_score + step
 
-    html=["<div align='center' style='width:100%'><div align='center' style='text-align:justify; overflow-x: scroll; width:70% !important; padding:10px; '; text-align: center; word-wrap: break-word;>"]
-    for idx,row in df.iterrows():
-        word=row.words.replace(" ","-")
-        scale=get_font_size(row.score)
-        color_code=get_color_code(row.score)
-        words_colors.append(" <span style='line-height:1.1em;color:{0};font-size:{1}em;font-family:georgia'>{2}&nbsp;</span>".format(color_code,scale,word))
-    
-    random.shuffle(words_colors)
+        return self.color_choices[0]
 
-    html.extend(words_colors)
-    html.append("</div></div>")
-    return ''.join(html)
+    def get_font_size(self, score:float):
+        """Increment scale until score almost equals current_incremented_score."""
+
+        # font size start and increment
+        scale = 0.6
+        max_scale = 3.0
+        scale_step = 0.3
+
+        # score increment
+        score_step = 0.05
+        current_incremented_score = 0
+
+
+        while current_incremented_score < 1 and scale < max_scale:
+
+            # increment scale until score almost equals current_incremented_score
+            # the larger the score, the more the scale increment
+            if score <= current_incremented_score:
+                return scale
+
+            current_incremented_score = current_incremented_score + score_step
+            scale += scale_step
+
+            if scale > max_scale:
+                scale = max_scale
+
+        return scale
+
+    def get_embed_code(self, text: list, topn=100, random_color=True):
+
+        items = self.extract_topn_from_vector(text, topn=topn)
+        new_df = pd.DataFrame(items, columns=['words', 'score'])
+
+        if random_color:
+            random.shuffle(self.color_choices)
+
+        html = []
+        words_colors = []
+
+        html = [
+            "<div align='center' style='width:100%'><div align='center' style='text-align:justify; border-radius: 25px;background: #fff7f7;overflow: auto; width:500px !important; padding:20px; '; text-align: center; word-wrap: break-word;>"]
+        for idx, row in new_df.iterrows():
+            word = row.words.replace(" ", "-")
+            scale = self.get_font_size(row.score)
+            color_code = self.get_color_code(row.score)
+            words_colors.append(
+                " <span style='color:{0};font-size:{1}em;white-space: normal;font-family:verdana;display: inline-block;line-height:20px'>{2}&nbsp;</span>".format(
+                    color_code, scale, word))
+
+        random.shuffle(words_colors)
+
+        html.extend(words_colors)
+        html.append("</div></div>")
+        return ''.join(html)
+
+    def sort_coo(self, coo_matrix):
+        tuples = zip(coo_matrix.col, coo_matrix.data)
+        return sorted(tuples, key=lambda x: (x[1], x[0]), reverse=True)
+
+    def get_stop_words(stop_file_path):
+        """load stop words """
+
+        with open(stop_file_path, 'r', encoding="utf-8") as f:
+            stopwords = f.readlines()
+            stop_set = set(m.strip() for m in stopwords)
+            return frozenset(stop_set)
+
+        # load a set of stop words
+        stopwords = get_stop_words("resources/stopwords.txt")
+
+    def extract_topn_from_vector(self, text: list, topn=10):
+
+        cv = CountVectorizer(stop_words='english')
+        word_count_vector = cv.fit_transform(text)  # generate tf-idf for the given document
+
+        big_text = ' '.join(text)
+
+        tfidf_transformer = TfidfTransformer(smooth_idf=True, use_idf=True)
+        tfidf_transformer.fit(word_count_vector)
+        tf_idf_vector = tfidf_transformer.transform(cv.transform([big_text]))
+
+        # sort the tf-idf vectors by descending order of scores
+        sorted_items = self.sort_coo(tf_idf_vector.tocoo())
+
+        # use only topn items from vector
+        sorted_items = sorted_items[:topn]
+
+        items = []
+
+        # word index and corresponding tf-idf score
+        for idx, score in sorted_items:
+            items.append([cv.get_feature_names()[idx], score])
+
+        return items
+
+wc=WordCloud()
+texts=['It works used works for any interable, not just lists. Disadvantage is that its not in place.','the','used','used','used','We now need to create the vocabulary vocabulary vocabulary vocabulary vocab vocab vocab and start the counting process. We can use the CountVectorizer to create a vocabulary from all the text in our']
+embed_code=wc.get_embed_code(texts,random_color=False,topn=100)
+print(embed_code)
